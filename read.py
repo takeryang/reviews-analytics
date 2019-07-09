848ab694c9fb15d7ac8ec9e9312cd3c30c86fb31
takeryang <takeryang@gmail.com>
2019-07-09 08:17:12 +0800

    2.0v

--- read.py
+++ read.py
@@ -8,7 +8,6 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 print('檔案讀取完了,總共有', len((data)), '筆資料')
 
-
 sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
@@ -22,3 +21,42 @@ for d in data:
 	if len(d) < 100:
 		new.append(d)
 print('一共有', len(new), '筆留言長度小於100')
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言提到good')
+
+
+ 
+#文字計數
+wc = {} # word_count
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1 #新增新的key進wc字典
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+print(len(wc))
+print(wc['Allen'])
+
+while True:
+	word = input('請問你想查什麼字: ')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現過的次數為', wc[word])
+	else:	
+		print('這個字沒有出現過喔!')
+
+print('感謝使用本查詢功能')
+
+
+
